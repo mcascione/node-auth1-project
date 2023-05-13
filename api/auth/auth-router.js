0ae99f806695e85769e2240cd360b4d5db21f2f4
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-12 17:47:17 -0700

    adjusted res.session.destroy to use req

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -40,7 +40,7 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
 
 router.get("/logout", (req, res, next) => {
   if (req.session.user) {
-    res.session.destroy((err) => {
+    req.session.destroy((err) => {
       if (err) {
         res.json({ message: "no session" });
       } else {
