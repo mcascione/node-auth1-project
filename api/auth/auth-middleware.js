cd391a9fdb5be8f22bba48027868d7c96a34d13c
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-12 17:12:19 -0700

    router and middleware completed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-
+const User = require("../users/users-model");
 /*
   If the user does not have a session saved in the server
 
@@ -7,8 +7,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
+  }
 }
 
 /*
@@ -19,8 +23,17 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) {
+      next();
+    } else {
+      next({ status: 422, message: "Username taken" });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -31,8 +44,18 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  try {
+    const user = await User.findBy({ username: req.body.username });
+    console.log(user);
+    if (user) {
+      next();
+    } else {
+      next({ status: 401, message: "Invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -43,14 +66,17 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length <= 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" });
+  } else {
+    next();
+  }
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
-  checkPasswordLength, 
-  checkUsernameExists, 
-  checkUsernameFree, 
-  restricted
-}
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+  restricted,
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,49 +1,68 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
-// const {
-//   checkUsernameFree,
-//   checkUsernameExists,
-//   checkPasswordLength,
-// } = require("./auth-middleware");
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+} = require("./auth-middleware");
+const User = require("../users/users-model");
 
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
+router.post(
+  "/register",
+  checkUsernameFree,
+  checkPasswordLength,
+  async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password, 12);
+      const newUser = { username, password: hash };
+      const result = await User.add(newUser);
+      res.status(200).json({
+        user_id: result.user_id,
+        username: result.username,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
+);
 
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
+router.post("/login", checkUsernameExists, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await User.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.json({ message: `Welcome ${user.username}!` });
+    } else {
+      next({
+        status: 401,
+        message: "invalid credentials",
+      });
+    }
+  } catch (err) {
+    next(err);
   }
+});
 
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    res.session.destroy((err) => {
+      if (err) {
+        res.json({ message: "no session" });
+      } else {
+        res.set(
+          "Set-Cookie",
+          "chocolatechip=; SameSite=Strict; Path=/; Expires=Thu, 01 Jan 1970 00:00:00"
+        );
+        res.json({ message: "logged out" });
+      }
+    });
+  } else {
+    next(res.json({ message: "no session" }));
   }
- */
-
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
+});
 
 /**
   3 [GET] /api/auth/logout
